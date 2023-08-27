b315d4272aeb3aaf95a480183e5c6e2a15df7ba6
Edna Vidal <ednavidalros@gmail.com>
2023-08-27 14:29:18 +0200

    "Main, app, homepage"

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,32 @@
-function HomePage() {}
+import { Link } from "react-router-dom"
+import beersImg from "./../assets/beers.png"
+import randomBeerImg from "./../assets/random-beer.png"
+import newBeerImg from "./../assets/new-beer.png"
+
+function HomePage() {
+
+    return (
+        <>
+            <ul>
+
+                <li><Link to={"/beers"}>
+                    <img src={beersImg} alt="Beers" />
+                    <h2>All Beers</h2>
+                    <p>Browse through our varied list of beers.</p>
+                </Link></li>
+                <li><Link to={"/random-beer"}>
+                    <img src={randomBeerImg} alt="Random beer" />
+                    <h2>Random Beer</h2>
+                    <p>Not sure what you want? Let us pick a beer for you.</p>
+                </Link></li>
+                <li><Link to={"/new-beer"}>
+                    <img src={newBeerImg} alt="New beer" />
+                    <h2>New Beer</h2>
+                    <p>Feeling inspired? Create your own beer!</p>
+                </Link></li>
+            </ul>
+        </>
+    )
+}
 
 export default HomePage;
